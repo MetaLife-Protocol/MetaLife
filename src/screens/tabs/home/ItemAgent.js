1129a250d606b5f229193fe5d9e52003bd51dac8
Lonmee <Lonmee@126.com>
2022-02-22 18:02:24 +0800

    + test

--- src/screens/tabs/home/ItemAgent.js
+++ src/screens/tabs/home/ItemAgent.js
@@ -3,28 +3,30 @@
  */
 import React from 'react';
 import PostItem from './items/PostItem';
+import {colorsSchema} from '../../../shared/SchemaStyles';
+import {Text} from 'react-native';
 
 const ItemAgent = ({item}) => {
   const {
     value: {author, timestamp, content},
   } = item;
   switch (content.type) {
-    // case 'contact':
-    //   return (
-    //     <Text style={{color: colorsSchema.primary}}>
-    //       {`${author.substring(0, 6)} ${
-    //         content.following ? '->' : '-<'
-    //       } ${content.contact.substring(0, 6)}`}
-    //     </Text>
-    //   );
-    // case 'about':
-    //   return (
-    //     <Text style={{color: colorsSchema.primary}}>
-    //       {`${content.about.substring(0, 6)} @> ${
-    //         content.name || content.description || content.image
-    //       }`}
-    //     </Text>
-    //   );
+    case 'contact':
+      return (
+        <Text style={{color: colorsSchema.primary}}>
+          {`${author.substring(0, 6)} ${
+            content.following ? '->' : '-<'
+          } ${content.contact.substring(0, 6)}`}
+        </Text>
+      );
+    case 'about':
+      return (
+        <Text style={{color: colorsSchema.primary}}>
+          {`${content.about.substring(0, 6)} @> ${
+            content.name || content.description || content.image
+          }`}
+        </Text>
+      );
     case 'post':
       return <PostItem item={item} />;
     default:
