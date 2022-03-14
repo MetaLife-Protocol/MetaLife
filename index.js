6aae77b2ed8960a1b89420cd4394554a59982817
Lonmee <Lonmee@126.com>
2022-03-14 16:26:25 +0800

    - nodejs

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ import React from 'react';
 import App from './src/App';
 import {persistor, store} from './src/store/configureStore';
 import {PersistGate} from 'redux-persist/integration/react';
-import nodejs from 'nodejs-mobile-react-native';
 
-setTimeout(() => {
-  const {start, channel} = nodejs;
-  channel.addListener('exception', console.warn);
-  channel.addListener('log4RN', console.log);
-  start('loader.js');
-}, 1);
+// setTimeout(() => {
+//   const {start, channel} = nodejs;
+//   channel.addListener('exception', console.warn);
+//   channel.addListener('log4RN', console.log);
+//   start('loader.js');
+// }, 1);
 
 const WrappedApp = () => {
   return (
