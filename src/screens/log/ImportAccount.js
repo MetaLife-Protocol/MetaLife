946710ea51f815ac49e5c75d5d1e382e1166f42e
pudgefury <jason@techlord.net>
2022-04-21 22:52:05 -0400

    import account tab bar

--- src/screens/log/ImportAccount.js
+++ src/screens/log/ImportAccount.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar, TextInput, View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
-import SchemaStyles, { colorsSchema } from '../../shared/SchemaStyles';
-import { connect } from 'react-redux/lib/exports';
+import React, {useState, useEffect} from 'react';
+import {
+  StatusBar,
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import SchemaStyles, {colorsSchema} from '../../shared/SchemaStyles';
+import {connect} from 'react-redux/lib/exports';
 import RoundBtn from '../../shared/comps/RoundBtn';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const iconDic = {
@@ -15,16 +23,16 @@ const iconDic = {
   Confirm_icon_selected: require('../../assets/image/accountBtn/Confirm_icon_selected.png'),
 };
 
-const Import = ({ name, setName }) => {
-  const { barStyle, BG, FG, flex1, input, text, marginTop10 } = SchemaStyles(),
-    { textHolder } = colorsSchema;
+const Import = ({name, setName}) => {
+  const {barStyle, BG, FG, flex1, input, text, marginTop10} = SchemaStyles(),
+    {textHolder} = colorsSchema;
 
   const [nick, setNick] = useState(''),
     [pwd, setPwd] = useState(''),
     [confirm, setConfirm] = useState(''),
-    { replace } = useNavigation();
+    {replace} = useNavigation();
 
-  const [mnemonic, setMnemonic] = useState('')
+  const [mnemonic, setMnemonic] = useState('');
   const [focusedClear, setfocusedClear] = useState(true);
   const [focusedDark, setfocusedDark] = useState(true);
   const [focusedConfirm, setfocusedConfirm] = useState(true);
@@ -32,31 +40,31 @@ const Import = ({ name, setName }) => {
   const cleaerPress = () => {
     setfocusedClear(!focusedClear);
     setNick('');
-  }
+  };
 
   return (
     <View style={[BG, flex1]}>
       <StatusBar barStyle={barStyle} />
-      <View style={[FG, flex1, marginTop10, styles.body]}>
+      <View style={[FG, flex1, marginTop10]}>
         <View>
-          <View style={[styles.tab]}>
-              <Text>
-              asdf
-              </Text>
-              <Text>
-              asdf
-              </Text>
-              <Text>
-              asdf
-              </Text>
-              <Text>
-              asdf
-              </Text>
+          <View style={[styles.tab, styles.body]}>
+            <View style={[styles.active]}>
+              <Text style={[styles.inputText, text]}>Mnemonic</Text>
+            </View>
+            <View>
+              <Text style={[styles.inputText]}>Keystore</Text>
+            </View>
+            <View>
+              <Text style={[styles.inputText]}>Private Key</Text>
+            </View>
+            <View>
+              <Text style={[styles.inputText]}>Observe</Text>
+            </View>
           </View>
         </View>
         <View style={flex1} />
         <RoundBtn
-          style={{ marginBottom: 50 }}
+          style={{marginBottom: 50}}
           title={'Start Importing'}
           disabled={!(nick && pwd && confirm && pwd == confirm)}
           press={() => createWallet()}
@@ -72,38 +80,48 @@ const msp = s => {
 
 const mdp = d => {
   return {
-    setName: name => d({ type: 'set', payload: name }),
-    deleteName: name => d({ type: 'delete' }),
+    setName: name => d({type: 'set', payload: name}),
+    deleteName: name => d({type: 'delete'}),
   };
 };
 
-
 const styles = StyleSheet.create({
   inputBox: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     fontSize: 20,
     borderBottomColor: 'black',
     borderBottomWidth: 1,
   },
   body: {
-    padding: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
   },
   inputText: {
     fontSize: 16,
+    color: "#8E8E92",
   },
   icon: {
     width: 20,
     height: 20,
   },
   tab: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
     borderBottomColor: 'black',
     borderBottomWidth: 1,
-  }
+    justifyContent: 'space-between',
+    paddingTop: 17.5,
+    paddingVertical:15,
+    paddingBottom: 0,
+  },
+  active: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#29DAD7',
+    paddingBottom: 10,
+  },
 });
 
 export default connect(msp, mdp)(Import);
