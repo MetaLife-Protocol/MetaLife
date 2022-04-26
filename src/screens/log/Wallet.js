fad4e656711bae8d08a800b8553b23ad0c9b4cc5
pudgefury <jason@techlord.net>
2022-04-26 17:19:53 +0900

    backup wallet modal

--- src/screens/log/Wallet.js
+++ src/screens/log/Wallet.js
@@ -1,16 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { StatusBar, TextInput, View, StyleSheet, Text, Image, TouchableOpacity, ImageBackground } from 'react-native';
-import SchemaStyles, { colorsSchema } from '../../shared/SchemaStyles';
-import { connect } from 'react-redux/lib/exports';
+import React, {useState, useEffect} from 'react';
+import {
+  StatusBar,
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  Modal,
+  Alert,
+} from 'react-native';
+import SchemaStyles, {colorsSchema} from '../../shared/SchemaStyles';
+import {connect} from 'react-redux/lib/exports';
 import RoundBtn from '../../shared/comps/RoundBtn';
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // import bip39 from 'react-native-bip39';
 // import bip32 from 'bip32';
 // import ethUtil from 'ethereumjs-util';
-import { ethers } from 'ethers';
-import { randomBytes } from 'react-native-randombytes'
-import { restrict } from '../../utils';
+import {ethers} from 'ethers';
+import {randomBytes} from 'react-native-randombytes';
+import {restrict} from '../../utils';
 
 const iconDic = {
   Clear_icon_default: require('../../assets/image/accountBtn/Clear_icon_default.png'),
@@ -21,50 +32,54 @@ const iconDic = {
   Confirm_icon_selected: require('../../assets/image/accountBtn/Confirm_icon_selected.png'),
 };
 
-const Wallet = ({ name, setName, currentAccount }) => {
-  const { barStyle, BG, FG, flex1, input, text, marginTop10 } = SchemaStyles(),
-    { textHolder } = colorsSchema;
+const Wallet = ({name, setName, currentAccount}) => {
+  const {barStyle, BG, FG, flex1, input, text, marginTop10, modalBackground} =
+      SchemaStyles(),
+    {textHolder} = colorsSchema;
 
   const [nick, setNick] = useState(''),
     [pwd, setPwd] = useState(''),
     [confirm, setConfirm] = useState(''),
     [tab, setTab] = useState(0),
-    { replace } = useNavigation();
+    {replace} = useNavigation();
 
-  const [mnemonic, setMnemonic] = useState('')
+  const [mnemonic, setMnemonic] = useState('');
   const [focusedClear, setfocusedClear] = useState(true);
   const [focusedDark, setfocusedDark] = useState(true);
   const [focusedConfirm, setfocusedConfirm] = useState(true);
+  const [confirmModal, setconfirmModal] = useState(true);
 
   const cleaerPress = () => {
     setfocusedClear(!focusedClear);
     setNick('');
-  }
+  };
 
   useEffect(() => {
     // randomBytes(16, (error, bytes) => {
     //   const mnemonic = ethers.utils.HDNode.entropyToMnemonic(bytes, ethers.wordlists.en);
     //   console.log(mnemonic);
     // });
-
   }, []);
 
   return (
     <View style={[BG, flex1]}>
       <StatusBar barStyle={barStyle} />
       <View style={[FG, flex1, marginTop10, styles.body]}>
-        <ImageBackground style={{
-          height: 156.5,
-        }}
-          imageStyle={{ borderRadius: 12 }}
+        <ImageBackground
+          style={{
+            height: 156.5,
+          }}
+          imageStyle={{borderRadius: 12}}
           source={require('../../assets/image/discover/Discover_backgroud_DAO.png')}>
           <View style={[styles.interface]}>
             <View style={[styles.interfaceHead]}>
               <Text>asdf</Text>
             </View>
             <View style={[styles.interfaceFooter]}>
-              <Text style={{ color: "#fff", fontSize: 13 }}>0x5D30E10741D1954BB5A4e88Ab7b15789b6df3420</Text>
-              <TouchableOpacity onPress={() => setfocusedDark(!focusedDark)}>
+              <Text style={{color: '#fff', fontSize: 13}}>
+                0x5D30E10741D1954BB5A4e88Ab7b15789b6df3420
+              </Text>
+              <TouchableOpacity onPress={() => setconfirmModal(true)}>
                 <Image
                   style={styles.icon}
                   source={require('../../assets/image/walletBtn/icon_copy_presses.png')}
@@ -76,21 +91,36 @@ const Wallet = ({ name, setName, currentAccount }) => {
         <View style={[styles.tab]}>
           <View style={[tab == 0 ? styles.active : null]}>
             <TouchableOpacity onPress={() => setTab(0)}>
-              <Text style={[styles.inputText, {color: "#8E8E92"}, tab == 0 ? text : null]}>
+              <Text
+                style={[
+                  styles.inputText,
+                  {color: '#8E8E92'},
+                  tab == 0 ? text : null,
+                ]}>
                 Coin
               </Text>
             </TouchableOpacity>
           </View>
-          <View style={[tab == 1 ? styles.active : null, { marginLeft: 24 }]}>
+          <View style={[tab == 1 ? styles.active : null, {marginLeft: 24}]}>
             <TouchableOpacity onPress={() => setTab(1)}>
-              <Text style={[styles.inputText, {color: "#8E8E92"},  tab == 1 ? text : null]}>
+              <Text
+                style={[
+                  styles.inputText,
+                  {color: '#8E8E92'},
+                  tab == 1 ? text : null,
+                ]}>
                 DAO
               </Text>
             </TouchableOpacity>
           </View>
-          <View style={[tab == 2 ? styles.active : null, { marginLeft: 24 }]}>
+          <View style={[tab == 2 ? styles.active : null, {marginLeft: 24}]}>
             <TouchableOpacity onPress={() => setTab(2)}>
-              <Text style={[styles.inputText, {color: "#8E8E92"},  tab == 2 ? text : null]}>
+              <Text
+                style={[
+                  styles.inputText,
+                  {color: '#8E8E92'},
+                  tab == 2 ? text : null,
+                ]}>
                 NFT
               </Text>
             </TouchableOpacity>
@@ -100,30 +130,102 @@ const Wallet = ({ name, setName, currentAccount }) => {
           <Text style={[styles.inputText]}>Comming soon...</Text>
         </View>
       </View>
-    </View >
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={confirmModal}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setconfirmModal(false);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={[styles.modalBack]}></View>
+          <View style={[styles.modalView, modalBackground]}>
+            <View style={styles.modalHeader}>
+              <Text style={[text, styles.modalText]}>
+                Please backup your wallet now
+              </Text>
+              <Text
+                style={[text, styles.modalText]}
+                onPress={() => setconfirmModal(false)}>
+                X
+              </Text>
+            </View>
+            <View style={styles.modalBody}>
+              <View style={styles.area}>
+                <Text style={{color: '#29DAD7', fontSize: 15}}>
+                  Unlike traditional website accounts, blockchain wallet is a
+                  decentralized account system based on cryptography.
+                </Text>
+                <Text style={{color: '#29DAD7', fontSize: 15, marginTop: 15}}>
+                  You must keep the private key and transaction password of your
+                  wallet. Any accident will lead to the loss of assets.
+                </Text>
+                <Text style={{color: '#29DAD7', fontSize: 15, marginTop: 15}}>
+                  We suggest to do a double backup first, then enter a small
+                  test, and finally start to use happily.
+                </Text>
+              </View>
+              <Text
+                style={[
+                  text,
+                  styles.modalText,
+                  {textAlign: 'left', marginTop: 15},
+                ]}>
+                Backup private key
+              </Text>
+              <Text
+                style={{textAlign: 'left', marginTop: 10, color: '#8E8E92'}}>
+                When you lose your wallet or forget your password, you can
+                restore your wallet
+              </Text>
+              <Text
+                style={[
+                  text,
+                  styles.modalText,
+                  {textAlign: 'left', marginTop: 15},
+                ]}>
+                Backing up keystore files
+              </Text>
+              <Text
+                style={{textAlign: 'left', marginTop: 10, color: '#8E8E92'}}>
+                Official wallet format, private key file protected by
+                transaction password
+              </Text>
+            </View>
+            <View style={styles.modalFooter}>
+              <RoundBtn
+                style={{width: '100%', marginHorizontal: 0}}
+                title={'Backup now'}
+                press={() => replace('Backup Wallet')}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
   );
 };
 
 const msp = s => {
   return {
-    currentAccount: s.account.currentAccount
+    currentAccount: s.account.currentAccount,
   };
 };
 
 const mdp = d => {
   return {
-    setName: name => d({ type: 'set', payload: name }),
-    deleteName: name => d({ type: 'delete' }),
+    setName: name => d({type: 'set', payload: name}),
+    deleteName: name => d({type: 'delete'}),
   };
 };
 
-
 const styles = StyleSheet.create({
   inputBox: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     fontSize: 20,
     borderBottomColor: 'black',
     borderBottomWidth: 1,
@@ -141,15 +243,14 @@ const styles = StyleSheet.create({
   },
   interface: {
     borderRadius: 15,
-    width: "100%",
+    width: '100%',
     height: 156.5,
     padding: 15,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between"
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
-  interfaceFooter: {
-  },
+  interfaceFooter: {},
   tab: {
     display: 'flex',
     marginTop: 10,
@@ -159,6 +260,69 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: '#29DAD7',
     paddingBottom: 5,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    borderRadius: 12,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    width: '90%',
+    zIndex: 2,
+  },
+  modalBody: {
+    width: '100%',
+    marginTop: 30,
+  },
+  modalText: {
+    textAlign: 'center',
+    fontSize: 17,
+  },
+  modalHeader: {
+    justifyContent: 'space-between',
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  modalTextDesc: {
+    color: '#4E586E',
+    fontSize: 15,
+    marginTop: 10,
+  },
+  modalFooter: {
+    marginTop: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  modalBack: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    zIndex: 1,
+  },
+  area: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    padding: 15,
+    borderRadius: 8,
   },
 });
 
