b16b36fad65a221aafcffc9099f4c9fdfd68f31c
‘liuqiang’ <liuqiang@deepthink.net.cn>
2022-04-28 16:54:41 +0800

    refactor:create NFT information

--- src/screens/nft/create_nft/comps/ConfirmNftInformationDialog.js
+++ src/screens/nft/create_nft/comps/ConfirmNftInformationDialog.js
@@ -10,6 +10,7 @@ import React from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {DialogTitle, RoundBtn, useDialog, useStyle} from 'metalife-base';
 import Toast from 'react-native-tiny-toast';
+import CreateNFTInformationView from './CreateNFTInfomationView';
 
 const ConfirmNftInformationDialog = ({
   file = {},
@@ -22,24 +23,11 @@ const ConfirmNftInformationDialog = ({
   return (
     <View style={styles.container}>
       <DialogTitle title={'NFT information'} />
-      <Image source={file} style={styles.img} />
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>your upload NFT :</Text>
-        <Text style={styles.value} numberOfLines={1} ellipsizeMode={'middle'}>
-          {file.fileName}
-        </Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>NFT name:</Text>
-        <Text style={styles.value} numberOfLines={1} ellipsizeMode={'middle'}>
-          {name}
-        </Text>
-      </View>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>NFT Description:</Text>
-        <View style={{flex: 1}} />
-      </View>
-      <Text style={styles.description}>{description}</Text>
+      <CreateNFTInformationView
+        description={description}
+        name={name}
+        file={file}
+      />
       <View style={styles.row}>
         <RoundBtn
           style={{paddingHorizontal: 20}}
@@ -89,31 +77,6 @@ const styleFun = theme =>
       padding: 15,
       alignItems: 'center',
       width: 345,
-    },
-    img: {
-      width: 100,
-      height: 100,
-      borderRadius: 10,
-      marginTop: 15,
-    },
-    title: {
-      fontSize: 15,
-      color: theme.c_4E586E,
-    },
-    value: {
-      fontSize: 15,
-      color: theme.c_000000_FFFFFF,
-      flex: 1,
-    },
-    description: {
-      fontSize: 15,
-      color: theme.c_000000_FFFFFF,
-      alignSelf: 'flex-start',
-    },
-    textContainer: {
-      flexDirection: 'row',
-      marginTop: 10,
-      alignSelf: 'flex-start',
     },
     row: {
       flexDirection: 'row',
