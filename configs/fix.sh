989da0546b2e2724882a56bad2eb0e345759d29d
‘liuqiang’ <liuqiang@deepthink.net.cn>
2022-05-06 19:23:26 +0800

    fix: sh fix recognizer podspec

---
+++ configs/fix.sh
@@ -0,0 +1,2 @@
+cp ./react-native-local-barcode-recognizer/RNReactNativeLocalBarcodeRecognizer.podspec ../node_modules/react-native-local-barcode-recognizer/ios/RNReactNativeLocalBarcodeRecognizer.podspec
+echo 'react-native-local-barcode-recognizer fixed done'
