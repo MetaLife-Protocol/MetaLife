8693876e5b3fd7a3de7e67977403594f204dfd00
Lonmee <Lonmee@126.com>
2022-05-27 13:31:14 +0800

    ^ Resync.js

--- src/screens/tabs/Profiles.js
+++ src/screens/tabs/Profiles.js
@@ -1,57 +1,25 @@
-import React, {useRef, useState, Suspense} from 'react';
+import React, {Suspense} from 'react';
 import {connect} from 'react-redux/lib/exports';
-import {Canvas, useFrame, useLoader} from '@react-three/fiber/native';
+import {useLoader} from '@react-three/fiber/native';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-
-function Box(props) {
-  const mesh = useRef(null);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={event => setActive(!active)}
-      onPointerOver={event => setHover(true)}
-      onPointerOut={event => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  );
-}
+import {Text} from 'react-native';
 
 const Profiles = ({avatar}) => {
   function Avatar(props) {
-    const avatarMesh = useRef(null);
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    //https://d1a370nemizbjq.cloudfront.net/e72d0bd0-de91-4d38-a7d2-0378823ccfd7.glb
-    const gltf = useLoader(GLTFLoader, require('../../assets/glb/avatar.glb'));
-    useFrame((state, delta) => (avatarMesh.current.rotation.y += 0.01));
-    return (
-      <mesh
-        {...props}
-        ref={avatarMesh}
-        scale={active ? 1.5 : 1}
-        onClick={event => setActive(!active)}
-        onPointerOver={event => setHover(true)}
-        onPointerOut={event => setHover(false)}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    );
+    // https://d1a370nemizbjq.cloudfront.net/e72d0bd0-de91-4d38-a7d2-0378823ccfd7.glb
+    // '../../assets/glb/avatar.glb'
+    const result = useLoader(GLTFLoader, '../../assets/glb/avatar.glb');
+    return <primitive object={result.scene} />;
+  }
+
+  function FallbackComponent() {
+    return <Text>loading...</Text>;
   }
 
   return (
-    <Canvas>
-      <Suspense fallback={null}>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Avatar />
-      </Suspense>
-    </Canvas>
+    <Suspense fallback={<FallbackComponent />}>
+      <Avatar />
+    </Suspense>
   );
 };
 
