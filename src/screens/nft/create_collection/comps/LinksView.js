3926f1b0b92d41c164e0d80d4cae25c50e75852d
Richard <siiii@qq.com>
2022-05-31 11:35:58 +0800

    style:create collection Links UI

--- src/screens/nft/create_collection/comps/LinksView.js
+++ src/screens/nft/create_collection/comps/LinksView.js
@@ -5,16 +5,86 @@
  * @desc:
  */
 
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {useStyle} from 'metalife-base';
+import React, {useCallback} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {PureTextInput, useStyle} from 'metalife-base';
 
-const LinksView = () => {
+const LinksView = ({netUrlCallback}) => {
   const styles = useStyle(createSty);
+
+  const itemView = useCallback(
+    ({
+      img,
+      headerUrl = '',
+      placeHolder,
+      onChange = () => {},
+      isNewLine = false,
+    }) => {
+      if (isNewLine) {
+        return (
+          <>
+            <View style={styles.row}>
+              <Image source={img} style={styles.icon} />
+              {!!headerUrl && (
+                <Text style={styles.headerUrlText}>{headerUrl}</Text>
+              )}
+            </View>
+            <PureTextInput
+              onChangeText={onChange}
+              style={[styles.inputStyle, {marginLeft: 30, marginTop: 2}]}
+              placeholder={placeHolder}
+            />
+          </>
+        );
+      }
+      return (
+        <View style={styles.row}>
+          <Image source={img} style={styles.icon} />
+          {!!headerUrl && <Text style={styles.headerUrlText}>{headerUrl}</Text>}
+          <PureTextInput
+            onChangeText={onChange}
+            style={styles.inputStyle}
+            placeholder={placeHolder}
+          />
+        </View>
+      );
+    },
+    [styles],
+  );
 
   return (
     <View style={styles.container}>
-      <Text>LinksView</Text>
+      {itemView({
+        img: require('../../../../assets/image/nft/icon_nft_website.png'),
+        placeHolder: 'Yoursite.io',
+        onChange: netUrlCallback,
+      })}
+      {itemView({
+        img: require('../../../../assets/image/nft/icon_nft_discord.png'),
+        headerUrl: 'https://discord.gg/',
+        placeHolder: 'abcdef',
+        onChange: netUrlCallback,
+      })}
+      {itemView({
+        img: require('../../../../assets/image/nft/icon_nft_instagram.png'),
+        headerUrl: 'https://www.instagra',
+        placeHolder: 'yourlmstarmHandle',
+        isNewLine: true,
+        onChange: netUrlCallback,
+      })}
+      {itemView({
+        img: require('../../../../assets/image/nft/icon_nft_medium.png'),
+        headerUrl: 'https://www.medium.com/@',
+        placeHolder: 'YourmediumHandle',
+        isNewLine: true,
+        onChange: netUrlCallback,
+      })}
+      {itemView({
+        img: require('../../../../assets/image/nft/icon_nft_telegram.png'),
+        headerUrl: 'https://t.me/',
+        placeHolder: 'abcdef',
+        onChange: netUrlCallback,
+      })}
     </View>
   );
 };
@@ -27,6 +97,25 @@ const createSty = theme =>
       borderRadius: 12,
       marginTop: 10,
       paddingHorizontal: 12,
+    },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+    },
+    icon: {
+      width: 20,
+      height: 20,
+    },
+    inputStyle: {
+      flex: 1,
+      marginLeft: 10,
+    },
+    headerUrlText: {
+      fontSize: 16,
+      color: theme.c_000000_FFFFFF,
+      marginLeft: 10,
+      lineHeight: 19,
     },
   });
 export default LinksView;
--- src/screens/nft/create_collection/index.js
+++ src/screens/nft/create_collection/index.js
@@ -6,12 +6,19 @@
  */
 
 import React, {useState} from 'react';
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
 import {PureTextInput, RoundBtn, useStyle} from 'metalife-base';
 import TitleAndTips from './comps/TitleAndTips';
 import ImagePickerView from './comps/ImagePickerView';
 import CategoryView from './comps/CategoryView';
 import LinksView from './comps/LinksView';
+import nativeDeviceInfo from 'react-native/Libraries/Utilities/NativeDeviceInfo';
 
 const CreateCollection = () => {
   const styles = useStyle(createSty);
@@ -22,92 +29,99 @@ const CreateCollection = () => {
     [description, setDescription] = useState(''),
     [category, setCategory] = useState(''),
     [creatorEarnings, setCreatorEarning] = useState(2.5);
+
+  const {isIPhoneX_deprecated} = nativeDeviceInfo.getConstants();
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={{marginHorizontal: 15}}>
-        <TitleAndTips
-          title={'Logo image'}
-          tips={
-            'This image will also be used for navigation. 350 x 350 recommended.'
-          }
-        />
-        <ImagePickerView
-          style={styles.logoContainer}
-          onImagePicker={setLogoImage}
-        />
-        <TitleAndTips
-          title={'Featured image'}
-          tips={
-            'This image will be used for featuring your collection on the homepage, category pages, or other promotional areas of OpenSea. 600 x 400 recommended.'
-          }
-        />
-        <ImagePickerView
-          style={styles.featuredContainer}
-          onImagePicker={setFeaturedImage}
-        />
-        <TitleAndTips
-          title={'Banner image'}
-          tips={
-            'This image will appear at the top of your collection page. Avoid including too much text in this banner image, as the dimensions change on different devices. 1400 x 400 recommended.'
-          }
-        />
-        <ImagePickerView
-          style={styles.featuredContainer}
-          onImagePicker={setBannerImage}
-        />
-        <TitleAndTips title={'Name'} />
-        <PureTextInput
-          placeholder={'Item name'}
-          style={styles.nameContainer}
-          onChangeText={setName}
-        />
-        <TitleAndTips
-          title={'Description'}
-          tips={'Markdown syntax is supported. 0 of 1000 characters used.'}
-        />
-        <PureTextInput
-          placeholder={'Provide a detalied description of you item'}
-          style={[styles.nameContainer, {height: 75}]}
-          onChangeText={setDescription}
-          inputProps={{multiline: true}}
-        />
-        <TitleAndTips
-          title={'Category'}
-          tips={
-            'Adding a category will help make your item discoverable on MetaLife.'
-          }
-        />
-        <CategoryView />
-        <TitleAndTips title={'Links'} />
-        <LinksView />
-        <TitleAndTips
-          title={'Creator Earnings'}
-          tips={
-            'Collect a fee when a user re-sells an item you originally created. This is deducted from the final sale price and paid monthly to a payout address of your choosing.\n' +
-            'Learn more about creator earnings.'
-          }
-        />
-        <PureTextInput
-          placeholder={'e.g.2.5'}
-          style={styles.nameContainer}
-          onChangeText={setCreatorEarning}
-        />
-        <TitleAndTips
-          title={'Blockchain'}
-          tips={
-            'Select the blockchain where you’s like new items from this  collection to be added by default.'
-          }
-        />
-        <TitleAndTips
-          title={'Payment tokens'}
-          tips={'These tokens can be used to buy and sell your items.'}
-        />
-        <TitleAndTips
-          title={'Display theme'}
-          tips={'Change how your items are shown.'}
-        />
-        <RoundBtn style={styles.buttonContainer} title={'Create'} />
-      </ScrollView>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={isIPhoneX_deprecated ? 94 : 64}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <ScrollView style={{marginHorizontal: 15}}>
+          <TitleAndTips
+            title={'Logo image'}
+            tips={
+              'This image will also be used for navigation. 350 x 350 recommended.'
+            }
+          />
+          <ImagePickerView
+            style={styles.logoContainer}
+            onImagePicker={setLogoImage}
+          />
+          <TitleAndTips
+            title={'Featured image'}
+            tips={
+              'This image will be used for featuring your collection on the homepage, category pages, or other promotional areas of OpenSea. 600 x 400 recommended.'
+            }
+          />
+          <ImagePickerView
+            style={styles.featuredContainer}
+            onImagePicker={setFeaturedImage}
+          />
+          <TitleAndTips
+            title={'Banner image'}
+            tips={
+              'This image will appear at the top of your collection page. Avoid including too much text in this banner image, as the dimensions change on different devices. 1400 x 400 recommended.'
+            }
+          />
+          <ImagePickerView
+            style={styles.featuredContainer}
+            onImagePicker={setBannerImage}
+          />
+          <TitleAndTips title={'Name'} />
+          <PureTextInput
+            placeholder={'Item name'}
+            style={styles.nameContainer}
+            onChangeText={setName}
+          />
+          <TitleAndTips
+            title={'Description'}
+            tips={'Markdown syntax is supported. 0 of 1000 characters used.'}
+          />
+          <PureTextInput
+            placeholder={'Provide a detalied description of you item'}
+            style={[styles.nameContainer, {height: 75}]}
+            onChangeText={setDescription}
+            inputProps={{multiline: true}}
+          />
+          <TitleAndTips
+            title={'Category'}
+            tips={
+              'Adding a category will help make your item discoverable on MetaLife.'
+            }
+          />
+          <CategoryView />
+          <TitleAndTips title={'Links'} />
+          <LinksView />
+          <TitleAndTips
+            title={'Creator Earnings'}
+            tips={
+              'Collect a fee when a user re-sells an item you originally created. This is deducted from the final sale price and paid monthly to a payout address of your choosing.\n' +
+              'Learn more about creator earnings.'
+            }
+          />
+          <PureTextInput
+            placeholder={'e.g.2.5'}
+            style={styles.nameContainer}
+            onChangeText={setCreatorEarning}
+          />
+          <TitleAndTips
+            title={'Blockchain'}
+            tips={
+              'Select the blockchain where you’s like new items from this  collection to be added by default.'
+            }
+          />
+          <TitleAndTips
+            title={'Payment tokens'}
+            tips={'These tokens can be used to buy and sell your items.'}
+          />
+          <TitleAndTips
+            title={'Display theme'}
+            tips={'Change how your items are shown.'}
+          />
+          <RoundBtn style={styles.buttonContainer} title={'Create'} />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
