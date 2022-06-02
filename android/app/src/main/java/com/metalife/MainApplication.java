9dbb1c77860edc00498e8639ef8de93cee774be3
Lonmee <Lonmee@126.com>
2022-06-02 17:23:44 +0800

    ^ avatar

--- android/app/src/main/java/com/metalife/MainApplication.java
+++ android/app/src/main/java/com/metalife/MainApplication.java
@@ -2,7 +2,6 @@ package com.metalife;
 
 import android.app.Application;
 import android.content.Context;
-import android.webkit.WebView;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
