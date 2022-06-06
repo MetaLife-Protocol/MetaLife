bf1a274df89c4ba33593b2497080894e97c32efa
Lonmee <Lonmee@126.com>
2022-06-06 18:04:56 +0800

    ^ avatar

--- src/screens/tabs/Profiles.js
+++ src/screens/tabs/Profiles.js
@@ -1,17 +1,18 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import {connect} from 'react-redux/lib/exports';
 import {WebView} from 'react-native-webview';
-import {Platform} from 'react-native';
+import {Button} from 'react-native';
 
 // (Platform.OS === 'ios') ? {uri: './FMDemoBaseMap/FMMapBasic.html'} : {uri: 'file:///android_asset/FMDemoBaseMap/FMMapBasic.html'}
 const Profiles = ({avatar}) => {
-  const webview = useRef();
+  const runFirst = `
+      document.body.style.backgroundColor = 'red';
+      window.ReactNativeWebView.onmessage = console.log;
+      setTimeout(function() { window.alert('hi') }, 2000);
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
 
-  useEffect(() => {
-    webview.current.postMessage(
-      JSON.stringify({fun: 'setUrl', params: {url: avatar}}),
-    );
-  }, [avatar]);
+  const webview = useRef();
 
   function loadHandler() {}
 
@@ -25,19 +26,36 @@ const Profiles = ({avatar}) => {
     console.log('processing: ', JSON.parse(data));
   }
 
+  function postMessage() {
+    webview.current.postMessage(
+      JSON.stringify({fun: 'setUrl', params: {url: avatar}}),
+    );
+  }
+
   return (
-    <WebView
-      ref={webview}
-      source={{uri: 'http://10.13.230.223:3000'}}
-      // source={
-      //   Platform.OS === 'ios'
-      //     ? require('../../assets/web/render/index.html')
-      //     : {uri: 'file:///android_asset/assets/web/render/index.html'}
-      // }
-      onLoad={loadHandler}
-      onLoadEnd={loadedHandler}
-      onMessage={messageHandler}
-    />
+    <>
+      <WebView
+        ref={webview}
+        source={{uri: 'http://10.13.230.223:3000'}}
+        // source={
+        //   Platform.OS === 'ios'
+        //     ? require('../../assets/web/render/index.html')
+        //     : {uri: 'file:///android_asset/assets/web/render/index.html'}
+        // }
+        onLoad={loadHandler}
+        onLoadEnd={loadedHandler}
+        onMessage={messageHandler}
+        injectedJavaScript={runFirst}
+      />
+      <Button
+        title={'post'}
+        onPress={() =>
+          webview.current.postMessage(
+            JSON.stringify({fun: 'setUrl', params: {url: avatar}}),
+          )
+        }
+      />
+    </>
   );
 };
 
