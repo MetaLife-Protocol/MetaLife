7884abca87abdd4cd1f78d201cead0a7be632722
Lonmee <Lonmee@126.com>
2022-07-06 11:54:26 +0800

    !^ keyboard

--- src/shared/comps/MsgInput.js
+++ src/shared/comps/MsgInput.js
@@ -19,9 +19,9 @@ const MsgInput = ({sendHandler}) => {
   const {FG, input, row, alignItemsCenter, flex1, text, placeholderTextColor} =
       useSchemaStyles(),
     {inner, round, textInput, sender} = styles;
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState(''),
+    [offset, setOffset] = useState(0);
   const {isIPhoneX_deprecated} = nativeDeviceInfo.getConstants();
-  const offset = isIPhoneX_deprecated ? 94 : 64;
   return (
     <KeyboardAvoidingView
       keyboardVerticalOffset={offset}
@@ -31,6 +31,8 @@ const MsgInput = ({sendHandler}) => {
           <TextInput
             style={[flex1, textInput, text]}
             placeholder="Write a comment …"
+            autoFocus={true}
+            onBlur={() => setOffset(isIPhoneX_deprecated ? 94 : 64)}
             value={content}
             onChangeText={setContent}
             placeholderTextColor={placeholderTextColor.color}
